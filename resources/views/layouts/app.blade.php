24a0d94b4ddcd27ed8c4ecc567e9a23f27705d88
Maximiliano Araujo <40903265+maximoaraujo@users.noreply.github.com>
2022-02-14 07:49:05 -0300

    menu accordion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -146,21 +146,46 @@
                 <!-- BEGIN: Aside Menu -->
                 <div id="m_ver_menu" class="m-aside-menu  m-aside-menu--skin-dark m-aside-menu--submenu-skin-dark " data-menu-vertical="true" data-menu-scrollable="false" data-menu-dropdown-timeout="500">
                     <ul class="m-menu__nav  m-menu__nav--dropdown-submenu-arrow ">
-                        <li class="m-menu__item  m-menu__item--active" aria-haspopup="true">
-                            <a href="{{ route('honorarios.calcular') }}" class="m-menu__link ">
-                                <i class="m-menu__link-icon flaticon-list-2"></i>
-                                <span class="m-menu__link-title">
-                                    <span class="m-menu__link-wrap">
-                                        <span class="m-menu__link-text">
-                                            Calcular honorarios
-                                        </span>
-                                    </span>
+                        <li class="m-menu__item  m-menu__item--submenu" aria-haspopup="true" data-menu-submenu-toggle="hover">
+                            <a href="#" class="m-menu__link m-menu__toggle">
+                                <i class="m-menu__link-icon flaticon-coins"></i>
+                                <span class="m-menu__link-text">
+                                    Honorarios
                                 </span>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <!-- END: Aside Menu -->
+                                <i class="m-menu__ver-arrow la la-angle-right"></i>
+                            </a>                              
+                            <div class="m-menu__submenu ">
+                                <span class="m-menu__arrow"></span>
+                                <ul class="m-menu__subnav">
+                                    <li class="m-menu__item " aria-haspopup="true">
+                                        <a href="{{ route('honorarios.calcular') }}" class="m-menu__link ">
+                                            <i class="m-menu__link-bullet m-menu__link-bullet--dot">
+                                                <span></span>
+                                            </i>
+                                            <span class="m-menu__link-text">
+                                                Calcular
+                                            </span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div><div class="m-menu__submenu ">
+                                <span class="m-menu__arrow"></span>
+                                <ul class="m-menu__subnav">
+                                    <li class="m-menu__item " aria-haspopup="true">
+                                        <a href="" class="m-menu__link ">
+                                            <i class="m-menu__link-bullet m-menu__link-bullet--dot">
+                                                <span></span>
+                                            </i>
+                                            <span class="m-menu__link-text">
+                                                Presupuestos
+                                            </span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>   
+                    </ul>  
+                </div>       
             </div>
             <!-- END: Left Aside -->
             <div class="m-grid__item m-grid__item--fluid m-wrapper">       
